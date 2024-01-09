2661a89dc72f37ea384ac7c19e99c7059e5f4292
husandro <149836402+MaxH6@users.noreply.github.com>
2024-01-09 21:43:06 +0100

    sono troppo forte
    massimo ha lavorato qundi puo quittare

--- Assets/Script/battleManager.cs
+++ Assets/Script/battleManager.cs
@@ -70,36 +70,79 @@ public class battleManager : MonoBehaviour
         enemy = e.GetComponent<enemyScript>();
         player = p.GetComponent<playerScript>();
 
+        //calcolazione del dmg player
+
         double playerAtk;
 
-        if (player.weaponIsMagic == false)
+        int playerHit;
+
+        if (player.weaponIsMagic == false){
 
             playerAtk = player.weaponMt + player.str;
 
-        /*else                                              //fai te il resto non ho voglia
-            playerAtk = weaponMt + mag;
+            playerHit=player.weaponHit+player.dex;
 
-        int efficace;
+        }
 
-        if (unitType == 1)
-            efficace = 3;
-        else if (unitType == 2)
-            efficace = 1;
-        else
-            efficace = 2;
+        else{  
 
-        int resistenza;
+            playerAtk = player.weaponMt + player.mag;
 
-        if (unitType == 1)
-            resistenza = 3;
-        else if (unitType == 2)
-            resistenza = 1;
-        else
-            resistenza = 2;
+            playerHit=player.weaponHit+(player.dex+player.lck)/2;
+
+        }
 
         double playerDmg;
 
-        if*/
+        if(player.unitEffective==enemy.unitType){
+
+            playerDmg*=1.5;
+
+        }
+        else if(enemy.unitType!=player.unitEffective and enemy.unitType!=player.unitType){
+
+            playerHit-=15;
+
+        }
+
+        double playerAS;
+
+        playerAS = player.spd - (player.weaponWt + )
+
+        //calcolazione del dmg enemy
+
+        double enemyAtk;
+
+        int enemyHit;
+
+        if (enemy.weaponIsMagic == false){
+
+            enemyAtk = enemy.weaponMt + enemy.str;
+
+            enemyHit=enemy.weaponHit + enemy.dex;
+
+        }
+
+        else{  
+
+            enemyAtk = enemy.weaponMt + enemy.mag;
+
+            enemyHit = enemy.weaponHit + (enemy.dex+enemy.lck)/2;
+
+        }
+
+        double enemyDmg;
+
+        if(enemy.unitEffective==player.unitType){
+
+            enemyDmg*=1.5;
+
+        }
+        else if(player.unitType!=enemy.unitEffective and player.unitType!=enemy.unitType){
+
+            enemyHit-=15;
+
+        }
     }
 
 
