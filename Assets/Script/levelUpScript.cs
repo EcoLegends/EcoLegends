6aa4d84ab0902a95da8e774373007b972a30766c
CreepersWizard <samuelecanc1@gmail.com>
2024-01-11 22:18:17 +0100

    level up anim

--- Assets/Script/levelUpScript.cs
+++ Assets/Script/levelUpScript.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class levelUpScript : MonoBehaviour
 {
@@ -46,9 +47,70 @@ public class levelUpScript : MonoBehaviour
         transform.GetChild(9).GetChild(0).transform.localScale = Vector3.zero;
 
         transform.GetChild(10).GetComponent<TextMeshProUGUI>().text = nome;
+
+        StartCoroutine(animazione());
     }
 
+    IEnumerator animazione()
+    {
+        AudioClip statsUp = (AudioClip)Resources.Load("Sounds/SFX/Stats Up");
+        AudioClip levelUp = (AudioClip)Resources.Load("Sounds/SFX/Level Up");
 
+        string[] names = { "HP", "Str", "Mag", "Dex", "Spd", "Lck", "Def", "Res" };
+        int[] values = { hp, str, mag, dex, spd, lck, def, res };
+
+        yield return new WaitForSeconds(1);
+
+        transform.GetChild(11).GetComponent<AudioSource>().PlayOneShot(levelUp);
+
+        for (float i = 1; i <= 100; i+=5)
+        {
+            transform.GetChild(9).GetChild(0).transform.localScale = new Vector3(i / 100, i / 100, i / 100);
+            if(i>=50) transform.GetChild(9).transform.localScale = new Vector3((i+50) / 100, (i+50) / 100, (i+50) / 100);
+            yield return new WaitForSeconds(0.01f);
+
+        }
+        transform.GetChild(9).GetComponent<TextMeshProUGUI>().text = (lvl + 1).ToString();
+        for (float i = 100; i >= 0; i-=5)
+        {
+            if (i >= 50) transform.GetChild(9).transform.localScale = new Vector3((i + 50) / 100, (i + 50) / 100, (i + 50) / 100);
+            transform.GetChild(9).GetChild(0).transform.localScale = new Vector3(i / 100, i / 100, i / 100);
+            yield return new WaitForSeconds(0.01f);
+
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+
+        for(int j = 0; j < 8; j++)
+        {
+            if (increments[j] == 1)
+            {
+                transform.GetChild(11).GetComponent<AudioSource>().PlayOneShot(statsUp);
+                for (float i = 1; i <= 100; i += 5)
+                {
+                    transform.GetChild(j+1).GetChild(0).transform.localScale = new Vector3(i / 100, i / 100, i / 100);
+                    if (i >= 50) transform.GetChild(j+1).transform.localScale = new Vector3((i + 50) / 100, (i + 50) / 100, (i + 50) / 100);
+                    yield return new WaitForSeconds(0.005f);
+
+                }
+                transform.GetChild(j+1).GetComponent<TextMeshProUGUI>().text = (names[j]+" "+(values[j] + 1)).ToString();
+                for (float i = 100; i >= 0; i -= 5)
+                {
+                    if (i >= 50) transform.GetChild(j+1).transform.localScale = new Vector3((i + 50) / 100, (i + 50) / 100, (i + 50) / 100);
+                    transform.GetChild(j+1).GetChild(0).GetChild(1).transform.localScale = new Vector3(i / 100, i / 100, i / 100);
+                    yield return new WaitForSeconds(0.005f);
+
+                }
+                yield return new WaitForSeconds(0.05f);
+            }
+            
+        }
+
+
+        yield return new WaitForSeconds(1);
+        Destroy(this.gameObject);
+    }
     void Update()
     {
         
